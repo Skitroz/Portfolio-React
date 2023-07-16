66ffc983a7ffad1484d90ef2e19644a06cf1dff6
Skitroz <65279654+Skitroz@users.noreply.github.com>
2023-07-17 00:20:41 +0200

    Update

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import Banniere from './header/banniere';
+
+function Header() {
+    return (
+        <Banniere telephone={"06 38 17 69 64"} mail={"contact@matheobatelier.fr"}/>
+    );
+}
+
+export default Header;
---
+++ src/components/header/banniere.js
@@ -0,0 +1,25 @@
+function Banniere({telephone, mail}) {
+    return (
+        <div className="bg-slate-700 py-6 px-[400px] text-white">
+            <nav className="flex justify-between">
+                <ul className="flex gap-5">
+                    <li className="flex items-center gap-2">
+                        <i className="fa-solid fa-phone fa-sg"></i>
+                        {telephone}
+                    </li>
+                    <li className="flex items-center gap-2">
+                        <i className="fa-solid fa-at fa-sg"></i>
+                        {mail}
+                    </li>
+                </ul>
+                <ul className="flex gap-5">
+                    <a href="https://github.com/Skitroz" target="_blank" rel="noreferrer"><i className="fa-brands fa-github fa-lg"></i></a>
+                    <a href="https://www.instagram.com/skitroz/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram fa-lg"></i></a>
+                    <a href="https://fr.linkedin.com/in/matheobtl" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin fa-lg"></i></a>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Banniere;
